48c89caec9e35c386ad3006f078d55c5bd366ab6
Mohansagar <mohansagar@accionlabs.com>
2022-08-27 15:41:41 +0530

    check point 1

---
+++ Problem1/README.md
@@ -0,0 +1,13 @@
+Steps to run the code
+
+Step #1: Open the hackathon folder in code editor and navigate to Problem 1 folder.
+
+Step #2: Open terminal and run the "npm install" command.
+
+Step #3: Goto Extensions tab in VS Code and install Code Runner extension.
+
+Step #4: Open index.js file and click "Ctrl + Alt + N" to run the code.
+
+Step #5: Open index.js file and click "Ctrl + Alt + M" to stop the code.
+
+Step #6: Check the output on the console.
---
+++ Problem1/index.js
@@ -0,0 +1,27 @@
+let selectedFile;
+console.log(window.XLSX);
+document.getElementById('input').addEventListener("change", (event) => {
+    selectedFile = event.target.files[0];
+})
+
+let data=[{
+    "name":"jayanth",
+    "data":"scd",
+    "abc":"sdef"
+}]
+
+
+document.getElementById('button').addEventListener("click", () => {
+    XLSX.utils.json_to_sheet(data, 'out.xlsx');
+    if(selectedFile){
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event)=>{
+         let data = "./Hackathon_Timesheet.xlsx";
+         let workbook = XLSX.read(data,{type:"binary"});
+
+         workbook.SheetNames.forEach(sheet => {
+              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+              console.log(rowObject);
+              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
+         });
